8fe2bc964a00c47164efbfe93d473876f4b8ad95
Tony <leonard.ant@gmail.com>
2014-08-23 19:34:21 +0100

    remove wrapping code as it seems to be added auto-magically!
    should be able to call by...
    org.apache.cordova.plugin.redraw.Redraw

--- www/redraw-plugin.js
+++ www/redraw-plugin.js
@@ -3,7 +3,7 @@ implementation demo from
 https://github.com/phonegap/phonegap-plugins/wiki/Defining-Your-Cordova-Plugin-As-A-Cordova-Module
 */
 
-cordova.define("cordova/plugin/redraw", function(require, exports, module) {
+//cordova.define("cordova/plugin/redraw", function(require, exports, module) {
 	var exec = require("cordova/exec");
 
 	var Redraw = function() {};
@@ -16,7 +16,7 @@ cordova.define("cordova/plugin/redraw", function(require, exports, module) {
     		exec(successCallback, errorCallback, "Redraw", "invalidateWebView", [delay]);
 	};
 
-	var myRedraw = new Redraw ();
+	//var myRedraw = new Redraw ();
 
-	module.exports = myRedraw;
-});
+	module.exports = Redraw;
+//});
